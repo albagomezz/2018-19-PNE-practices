32e0b7297f86d47f6ba0ff04d47076f0d86983e4
albagomezz <yo.alba.g@gmail.com>
2019-03-05 16:15:38 +0100

    session 13

---
+++ session-13/webserver1-test.py
@@ -0,0 +1,33 @@
+import http.server
+import socketserver
+
+# Define the Server's port
+PORT = 8003
+
+class TestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        print("GET received")
+        print("Request line:" + self.requestline)
+        print("   Cmd: " + self.command)
+        print("   Path: " + self.path)
+
+        content = "I am the happy server :)"
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.send_header("Content-Length", len(str.encode(content)))
+        self.end_headers()
+        self.wfile.write(str.encode(content))
+        return
+
+
+# -- Use the http.server Handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    httpd.serve_forever()
