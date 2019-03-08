f90d0cd56686b13d8721296b64ab70414d499584
albagomezz <yo.alba.g@gmail.com>
2019-03-08 15:41:23 +0100

    practice 1

---
+++ P1/Seq.py
@@ -0,0 +1,16 @@
+
+
+class Seq:
+    def __init__(seq):
+        print("The sequence is: {}".format(seq))
+
+    def len(seq):
+
+    def complement(seq):
+
+    def reverse(seq):
+
+    def count(base):
+
+    def perc(base):
+
