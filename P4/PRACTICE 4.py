f301be35b05c85dd6348b49041957ced3d044e62
albagomezz <yo.alba.g@gmail.com>
2019-03-13 18:01:25 +0100

    practice 4

--- P4/PRACTICE 4.py
+++ P4/PRACTICE 4.py
@@ -1,7 +1,83 @@
 import socket
 import termcolor
-#own ip adress
-IP = "212.128.253.86"
-PORT = 8080
+
+IP = "212.128.253.78"
+PORT = 8083
 MAX_OPEN_REQUESTS = 5
 
+
+def process_client(cs):
+    """Process the client request.
+    Parameters:  cs: socket for communicating with the client"""
+
+    # Read client message. Decode it as a string
+    msg = cs.recv(2048).decode("utf-8")
+
+    # Print the received message, for debugging
+    print()
+    print("Request message: ")
+    termcolor.cprint(msg, 'green')
+
+    # Build the HTTP response message. It has the following lines
+    # Status line
+    # header
+    # blank line
+    # Body (content to send)
+
+    # This new contents are written in HTML language
+    contents = """
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title>This is the index</title>
+      </head>
+      <body style="background-color: yellow;">
+        <h1>INDEX SERVER</h1>
+        <a href="blue.html">Blue server</a>
+      </body>
+    </html>
+    """
+
+    # -- Everything is OK
+    status_line = "HTTP/1.1 200 OK\r\n"
+
+    # -- Build the header
+    header = "Content-Type: text/html\r\n"
+    header += "Content-Length: {}\r\n".format(len(str.encode(contents)))
+
+    # -- Build the message by joining together all the parts
+    response_msg = str.encode(status_line + header + "\r\n" + contents)
+    cs.send(response_msg)
+
+    # Close the socket
+    cs.close()
+
+
+# MAIN PROGRAM
+
+# create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the IP and PORT
+serversocket.bind((IP, PORT))
+
+# Configure the server sockets
+# MAX_OPEN_REQUESTS connect requests before refusing outside connections
+serversocket.listen(MAX_OPEN_REQUESTS)
+
+print("Socket ready: {}".format(serversocket))
+
+while True:
+    # accept connections from outside
+    # The server is waiting for connections
+    print("Waiting for connections at {}, {} ".format(IP, PORT))
+    (clientsocket, address) = serversocket.accept()
+
+    # Connection received. A new socket is returned for communicating with the client
+    print("Attending connections from client: {}".format(address))
+
+    # Service the client
+    process_client(clientsocket)
+
+# como hago q cambie de pagina? del index a la azul por ejemplo
