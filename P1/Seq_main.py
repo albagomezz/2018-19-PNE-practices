5929e4144b3aca51b93ae64b0fc1cf6ab5faf781
albagomezz <yo.alba.g@gmail.com>
2019-03-14 18:39:56 +0100

    practice 1 in process

--- P1/Seq_main.py
+++ P1/Seq_main.py
@@ -11,8 +11,8 @@ s4 = s3.reverse()
 sequences = [s1, s2, s3, s4]
 
 for elem in sequences:
-    print("Sequence 1: ", s1)
-    print(" Length:{}".format(s1.len()))
+    print("Sequence 1: ", elem)
+    print(" Length:{}".format(elem.len()))
     print(" Bases count: A: {}".format(elem.count('A')), "T: {}".format(elem.count('T')), "C: {}".format(elem.count('C')), "G: {}".format(elem.count('G')))
     print(" Bases percentage: A: {}%".format(elem.perc('A')), "T: {}%".format(elem.perc('T')), "C: {}%".format(elem.perc('C')), "G: {}%".format(elem.perc('G')))
 
