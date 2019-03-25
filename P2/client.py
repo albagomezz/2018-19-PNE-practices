eb792b4acc7533752df8735ebf6772330383a3f6
albagomezz <yo.alba.g@gmail.com>
2019-03-25 17:01:49 +0100

    practice 2

--- P2/client.py
+++ P2/client.py
@@ -1,26 +1,39 @@
-# programming our first client
+#first we define the class
+
+class Seq:
+    def __init__(self, strbases):
+        self.strbases = strbases
+
+    def complement(self):
+        bases_c = ""
+        change = {"A": "T", "T": "A", "G": "C", "C": "G"}
+        for i in self.strbases:
+            bases_c += change[i]
+        return bases_c
+
+    def reverse(self):
+        r = self.strbases[::-1]
+        return Seq(r)
+
+
+#then we create our main program
 
 import socket
 
-# create a socket for communicating with the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connected = True
+while connected:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket created")
 
-print("Socket created")
+    PORT = 8081
+    IP = "212.128.253.105"
 
-PORT = 8080
-IP = "212.128.253.93"
+    s.connect((IP, PORT))
 
-s.connect((IP, PORT))
+    s1 = Seq(input("Introduce the sequence to obtain the reverse-complement sequence: "))
+    s2 = Seq(s1.reverse()).complement()
+    print("The reverse-complement sequence is: ", s2)
 
-s.send(str.encode("hola"))
-
-msg = s.recv(2048).decode("utf-8")
-
-print("MESSAGE FORM THE SERVER: ")
-
-
-print(msg)
-
-s.close()
-
-print("The end")
+    s.send(str.encode(s2))
+    s.close()
+    print("The sequence has been sent to the server")
--- P2/server.py
+++ P2/server.py
@@ -1,8 +1,8 @@
 import socket
 
 # Configure the Server's IP and PORT
-PORT = 8080
-IP = "212.128.253.93"
+PORT = 8081
+IP = "212.128.253.105"
 MAX_OPEN_REQUESTS = 5
 
 # Counting the number of connections
