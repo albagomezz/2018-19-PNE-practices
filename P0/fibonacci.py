c4d3c2be30260ecead47933ebbdb60b02e34674b
albagomezz <yo.alba.g@gmail.com>
2019-03-25 15:40:54 +0100

    practice 0

--- P0/fibonacci.py
+++ P0/fibonacci.py
@@ -1,6 +1,18 @@
 def fibonacci(n):
-    for a in range(n+1):
-        x = fibonacci(a-1)+fibonacci(a-2)
-        return x
-num = int(input("Enter the number of term: "))
-print(fibonacci(num))
+    n1 = n3 = 0
+    n2 = 1
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        for i in range(n - 2):
+            n3 = n1 + n2
+            n1 = n2
+            n2 = n3
+        return n3
+
+
+number = int(input("Introduce the number: "))
+result = fibonacci(number)
+print("The result for the number introduced is: {}".format(result))
