f70e9f8edf4c0f748b038a6ccc9528c005da9895
albagomezz <yo.alba.g@gmail.com>
2019-03-26 16:21:55 +0100

    Chuck Norris database

---
+++ session-19/exercice1.py
@@ -0,0 +1,133 @@
+# Accessing to the Chuck Norris database service for getting the number of total jokes,
+# the number and names of the categories and a random joke
+
+import http.client
+import json
+
+# -- API information
+HOSTNAME = "api.icndb.com"
+ENDPOINT = "/jokes/count"
+METHOD = "GET"
+
+# -- Here we can define special headers if needed
+headers = {'User-Agent': 'http-client'}
+
+# -- Connect to the server
+# -- NOTICE it is an HTTPS connection!
+# -- If we do not specify the port, the standar one
+# -- will be used
+conn = http.client.HTTPConnection(HOSTNAME)  #important check if here we have HTTPConnection or HTTPSConnection. If it doesnt work, try the other
+
+# -- Send the request. No body (None)
+# -- Use the defined headers
+conn.request(METHOD, ENDPOINT, None, headers)
+
+# -- Wait for the server's response
+r1 = conn.getresponse()
+
+# -- Print the status
+print()
+print("Response received: ", end='')
+print(r1.status, r1.reason)
+
+# -- Read the response's body and close
+# -- the connection
+text_json = r1.read().decode("utf-8")
+conn.close()
+
+# -- Optionally you can print the
+# -- received json file for testing
+# print(text_json)
+
+# -- Generate the object from the json file
+number = json.loads(text_json)
+
+# -- Print the received URL
+print("The total number of jokes is:", number['value'])
+
+# -- API information
+HOSTNAME = "api.icndb.com"
+ENDPOINT = "/categories"
+METHOD = "GET"
+
+# -- Here we can define special headers if needed
+headers = {'User-Agent': 'http-client'}
+
+# -- Connect to the server
+# -- NOTICE it is an HTTPS connection!
+# -- If we do not specify the port, the standar one
+# -- will be used
+conn = http.client.HTTPConnection(HOSTNAME)  #important check if here we have HTTPConnection or HTTPSConnection. If it doesnt work, try the other
+
+# -- Send the request. No body (None)
+# -- Use the defined headers
+conn.request(METHOD, ENDPOINT, None, headers)
+
+# -- Wait for the server's response
+r1 = conn.getresponse()
+
+# -- Print the status
+print()
+print("Response received: ", end='')
+print(r1.status, r1.reason)
+
+# -- Read the response's body and close
+# -- the connection
+text_json = r1.read().decode("utf-8")
+conn.close()
+
+# -- Optionally you can print the
+# -- received json file for testing
+# print(text_json)
+
+# -- Generate the object from the json file
+categories = json.loads(text_json)
+num = len(categories)
+
+# -- Print the received URL
+print("There are {} categories".format(num))
+print("The categories of the jokes are:")
+for i in categories:
+    print (i)
+
+
+# -- API information
+HOSTNAME = "api.icndb.com"
+ENDPOINT = "/jokes/random"
+METHOD = "GET"
+
+# -- Here we can define special headers if needed
+headers = {'User-Agent': 'http-client'}
+
+# -- Connect to the server
+# -- NOTICE it is an HTTPS connection!
+# -- If we do not specify the port, the standar one
+# -- will be used
+conn = http.client.HTTPConnection(HOSTNAME)  #important check if here we have HTTPConnection or HTTPSConnection. If it doesnt work, try the other
+
+# -- Send the request. No body (None)
+# -- Use the defined headers
+conn.request(METHOD, ENDPOINT, None, headers)
+
+# -- Wait for the server's response
+r1 = conn.getresponse()
+
+# -- Print the status
+print()
+print("Response received: ", end='')
+print(r1.status, r1.reason)
+
+# -- Read the response's body and close
+# -- the connection
+text_json = r1.read().decode("utf-8")
+conn.close()
+
+# -- Optionally you can print the
+# -- received json file for testing
+# print(text_json)
+
+# -- Generate the object from the json file
+random = json.loads(text_json)
+
+print("Here is a random joke:")
+print(random['value'].get("joke"))
