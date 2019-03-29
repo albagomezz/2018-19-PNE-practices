3d2d61108e6a4f22a8f8ecb2e45cd42b9c6844c8
albagomezz <yo.alba.g@gmail.com>
2019-03-29 18:04:02 +0100

    almost finished

--- session-19/exercice3.py
+++ session-19/exercice3.py
@@ -7,8 +7,7 @@ user = input("Write the user to search: ")
 # -- API information
 HOSTNAME = "api.github.com"
 ENDPOINT1 = "/users/"
-ENDPOINT2 = "/repos"
-GITHUB_ID = "{}".format(user)
+GITHUB_ID = user
 METHOD = "GET"
 
 # -- Here we can define special headers if needed
@@ -42,48 +41,63 @@ conn.close()
 # print(text_json)
 
 # -- Generate the object from the json file
-user = json.loads(text_json)
+u = json.loads(text_json)
 
 # -- Get some data
-name = user['name']
-bio = user['bio']
-nrepos = user['public_repos']
+name = u['name']
+nrepos = u['public_repos']
+repos = user['repos_url']
+
+def all_repos(link):
+    HOSTNAME = "api.github.com"
+    ENDPOINT2 = link[22:]
+    METHOD = "GET"
+
+    # -- Here we can define special headers if needed
+    headers = {'User-Agent': 'http-client'}
+
+    # -- Connect to the server
+    # -- NOTICE it is an HTTPS connection!
+    # -- If we do not specify the port, the standar one
+    # -- will be used
+    conn = http.client.HTTPSConnection(HOSTNAME)
+
+    # -- Send the request. No body (None)
+    # -- Use the defined headers
+    conn.request(METHOD, ENDPOINT2, None, headers)
+
+    # -- Wait for the server's response
+    r1 = conn.getresponse()
+
+    # -- Print the status
+    print()
+    print("Response received: ", end='')
+    print(r1.status, r1.reason)
+
+    # -- Read the response's body and close
+    # -- the connection
+    text_json = r1.read().decode("utf-8")
+    conn.close()
+
+    # -- Optionally you can print the
+    # -- received json file for testing
+    # print(text_json)
+
+    # -- Generate the object from the json file
+    info = json.loads(text_json)
+    rep = []
+    commits = len(info[0]['commits_url'])
+    for i in info:
+        rep.append(i['name'])
+    add_to_list = ""
+    for a in rep:
+        add_to_list = add_to_list + a + ','
+    return commits, add_to_list
+
+information, comm = all_repos(repos)
 
 print()
-print("Real Name: {}".format(name))
-print("Repos: {}".format(nrepos))
-
-
-# -- Here we can define special headers if needed
-headers = {'User-Agent': 'http-client'}
-
-# -- Connect to the server
-# -- NOTICE it is an HTTPS connection!
-# -- If we do not specify the port, the standar one
-# -- will be used
-conn = http.client.HTTPSConnection(HOSTNAME)
-
-# -- Send the request. No body (None)
-# -- Use the defined headers
-conn.request(METHOD, ENDPOINT1 + GITHUB_ID + ENDPOINT2, None, headers)
-
-# -- Wait for the server's response
-r1 = conn.getresponse()
-
-# -- Print the status
-print()
-print("Response received: ", end='')
-print(r1.status, r1.reason)
-
-# -- Read the response's body and close
-# -- the connection
-text_json = r1.read().decode("utf-8")
-conn.close()
-
-# -- Optionally you can print the
-# -- received json file for testing
-# print(text_json)
-
-# -- Generate the object from the json file
-user = json.loads(text_json)
+print("Real name:", name)
+print("Names of all the repos: ", information)
+print("Number of commits to the 2018-19-PNE-repo", comm)
 
