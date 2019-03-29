56f1b0f8c7f72762f2bec9aa65cb4764e63c2977
albagomezz <yo.alba.g@gmail.com>
2019-03-29 17:30:10 +0100

    finished

--- session-19/exercice2.py
+++ session-19/exercice2.py
@@ -1,17 +1,57 @@
-# Example of getting information about the weather of
-# a location
-
+#We are going to search the woeid of the place and then show the weather for that place
 import http.client
 import json
 
-# -- API information
-HOSTNAME = "www.metaweather.com"
-ENDPOINT = "/api/location/search/?query="
 
-LOCATION_WOEID = "london"
+def woeid(place):
+    HOSTNAME = "www.metaweather.com"
+    ENDPOINT = "/api/location/search/?query="
+    LOCATION_WOEID = ENDPOINT + place
+    METHOD = "GET"
+
+    headers = {'User-Agent': 'http-client'}
+
+    # -- Connect to the server
+    # -- NOTICE it is an HTTPS connection!
+    # -- If we do not specify the port, the standar one
+    # -- will be used
+    conn = http.client.HTTPSConnection(HOSTNAME)
+
+    # -- Send the request. No body (None)
+    # -- Use the defined headers
+    conn.request(METHOD, LOCATION_WOEID, None, headers)
+
+    # -- Wait for the server's response
+    r1 = conn.getresponse()
+
+    # -- Print the status
+    print()
+    print("Response received: ", end='')
+    print(r1.status, r1.reason)
+
+    # -- Read the response's body and close
+    # -- the connection
+    text_json = r1.read().decode("utf-8")
+    conn.close()
+
+    # -- Optionally you can print the
+    # -- received json file for testing
+    # print(text_json)
+
+    # -- Generate the object from the json file
+    data = json.loads(text_json)
+
+    # -- Get the data
+    w = data[0]['woeid']
+
+    return w
+
+HOSTNAME = "www.metaweather.com"
+ENDPOINT = "/api/location/"
+place = input("Introduce the name of the place: ")
+LOC = str(woeid(place))
 METHOD = "GET"
 
-# -- Here we can define special headers if needed
 headers = {'User-Agent': 'http-client'}
 
 # -- Connect to the server
@@ -22,7 +62,7 @@ conn = http.client.HTTPSConnection(HOSTNAME)
 
 # -- Send the request. No body (None)
 # -- Use the defined headers
-conn.request(METHOD, ENDPOINT + LOCATION_WOEID + '/', None, headers)
+conn.request(METHOD, ENDPOINT + LOC + '/', None, headers)
 
 # -- Wait for the server's response
 r1 = conn.getresponse()
@@ -37,15 +77,19 @@ print(r1.status, r1.reason)
 text_json = r1.read().decode("utf-8")
 conn.close()
 
-# -- Optionally you can print the
-# -- received json file for testing
-# print(text_json)
-
 # -- Generate the object from the json file
 weather = json.loads(text_json)
 
 # -- Get the data
-place = weather[0]['title']
+time = weather['time']
+
+temp0 = weather['consolidated_weather'][0]
+description = temp0['weather_state_name']
+temp = temp0['the_temp']
+place = weather['title']
 
 print()
 print("Place: {}".format(place))
+print("Time: {}".format(time))
+print("Weather description: {}".format(description))
+print("Current temp: {} degrees".format(temp))
