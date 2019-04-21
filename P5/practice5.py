a14cb6d65945adb4247da3ce694bd79b5acbbd7d
albagomezz <a.gomezde.2018@alumnos.urjc.es>
2019-04-21 22:15:39 +0200

    p5

--- P5/practice5.py
+++ P5/practice5.py
@@ -1,85 +1,51 @@
 import http.server
 import socketserver
-import termcolor
 
-# Define the Server's port
-PORT = 8009
+PORT = 8002
 
-
-# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
-# It means that our class inheritates all his methods and properties
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
-        """This method is called whenever the client invokes the GET method
-        in the HTTP protocol request"""
+        print("GET received")
 
-        # Print the request line
-        termcolor.cprint(self.requestline, 'green')
+        print("Request line:" + self.requestline)
+        print("     Cmd: "+self.command)
+        print("     Path: "+self.path)
 
-        # IN this simple server version:
-        # We are NOT processing the client's request
-        # It is a happy server: It always returns a message saying
-        # that everything is ok
-
-        # Message to send back to the client
-        c = self.requestline.splitlines()
-        c = c[0].lstrip("GET ").rstrip(" HTTP/1.1")
-        if c == "/":
-            f = open("index.html", "r")
+        c = self.requestline.split(" ")
+        if c[1] == '/':
+            f = open('index.html', 'r')
             contents = f.read()
             f.close()
 
-        elif c == "/pink":
-            f = open("pink.html", "r")
+        elif c == '/pink':
+            f = open('pink.html', 'r')
             contents = f.read()
             f.close()
-            
-        elif c == "/blue":
-            f = open("blue.html", "r")
+
+        elif c == '/blue':
+            f = open('blue.html', 'r')
             contents = f.read()
             f.close()
 
         else:
-            f = open("error.html", "r")
+            f = open('error.html', 'r')
             contents = f.read()
             f.close()
-
-        # Generating the response message
-        self.send_response(200)  # -- Status line: OK!
-
-        # Define the content-type header:
-        self.send_header('Content-Type', 'text/plain')
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
         self.send_header('Content-Length', len(str.encode(contents)))
-
-        # The header is finished
         self.end_headers()
 
-        # Send the response message
         self.wfile.write(str.encode(contents))
 
         return
 
 
-# ------------------------
-# - Server MAIN program
-# ------------------------
-# -- Set the new handler
 Handler = TestHandler
 
-# -- Open the socket server
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print('Serving at PORT', PORT)
 
-    print("Serving at PORT", PORT)
+    httpd.serve_forever()
 
-    # -- Main loop: Attend the client. Whenever there is a new
-    # -- clint, the handler is called
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        print("")
-        print("Stoped by the user")
-        httpd.server_close()
-
-print("")
-print("Server Stopped")
