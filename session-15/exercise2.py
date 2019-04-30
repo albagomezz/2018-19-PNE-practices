31cbaea796a5edaab7ba9824117191bf640ae219
albagomezz <a.gomezde.2018@alumnos.urjc.es>
2019-04-30 18:47:39 +0200

    ex2 finished

--- session-15/exercise2.py
+++ session-15/exercise2.py
@@ -4,7 +4,7 @@ import termcolor
 
 # we are using http library (always from now on)
 # Define the Server's port
-PORT = 8002
+PORT = 8003
 
 
 # Class with our Handler. It is a called derived from BaseHTTPRequestHandler
@@ -15,17 +15,21 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         # Print the request line
         termcolor.cprint(self.requestline, 'green')
 
-        if self.path.startswith("/main-page")or self.path == "/":
-            f = open("form1.html", "r")
+        if self.path.startswith("/main-page") or self.path == "/":
+            f = open("form3.html", "r")
             contents = f.read()
-        elif self.path.startswith("/echo"):
-            msg= self.path.split("=")
+        elif self.path.startswith("/myserver"):
+            msg = self.path.split("=")
             ff = open("echo.html", "r")
             contents = ff.read()
-            contents += msg[1]
+            if "on" in msg:
+                contents += msg[1].upper()
+                contents = contents[:-4]
+            elif "on" not in msg:
+                contents += msg[1]
             contents += """</p>
                                         <a href="main-page">The main page</a>
-    
+
                                     </body>
                                     </html>
                                     """
@@ -37,7 +41,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         self.send_response(200)  # -- Status line: OK!
 
         # Define the content-type header:
-        self.send_header('Content-Type', 'text/html') #this header is crucial
+        self.send_header('Content-Type', 'text/html')  # this header is crucial
         self.send_header('Content-Length', len(str.encode(contents)))
 
         # The header is finished
@@ -57,7 +61,6 @@ Handler = TestHandler
 
 # -- Open the socket server
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-
     print("Serving at PORT", PORT)
 
     # -- Main loop: Attend the client. Whenever there is a new
